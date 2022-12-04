708f80659be3a99a2e6a32af343118da8fbcbf60
Ucheawaji Sunday <ucheawaji@ext.kobo360.com>
2022-12-05 00:29:57 +0100

    feat: add authentication system

--- real_estate/settings/development.py
+++ real_estate/settings/development.py
@@ -11,3 +11,14 @@ DATABASES = {
         'PORT': env('DB_PORT'),
     }
 }
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = "info@real_estate.com"
+SITE_NAME = "Real Estate"
+
+
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = env("EMAIL_PORT")
+DOMAIN = env("DOMAIN")
